0b8d09eb1f2a6396679cbd83985b5f184aa072a4
Your Name <mariaeduarda.dan.avila@gmail.com  git config --global user.email you@example.com  git config --global user.name DudaAvila>
2025-10-30 18:21:54 -0300

    UPDATE

--- java/movimentos.js
+++ java/movimentos.js
@@ -1,6 +1,40 @@
-const container=document.getElementById("container"); 
-const player= document.createElement("div");
-player.id="player"; 
-container.appendChild(player); 
+const container = document.getElementById("container");
+const player = document.getElementById("player");
 
-let playerx= 180; 
+let playerX = 180; // posição inicial no eixo X
+let playerY = 100; // posição inicial no eixo Y
+
+player.style.top = playerY + "px";
+player.style.left = playerX + "px";
+
+const step = 40;   // tamanho do movimento
+const limitMin = 0;
+const limitMax = 400;
+
+document.addEventListener("keydown", (e) => {
+
+    switch (e.key) {
+
+        case "ArrowLeft":
+            if (playerX > limitMin) playerX -= step;
+            break;
+
+        case "ArrowRight":
+            if (playerX < limitMax) playerX += step;
+            break;
+
+        case "ArrowUp":
+            if (playerY > limitMin) playerY -= step;
+            break;
+
+        case "ArrowDown":
+            if (playerY < limitMax) playerY += step;
+            break;
+    }
+
+    player.style.top = playerY + "px";
+    player.style.left = playerX + "px";
+
+    console.log(playerX, playerY);
+});
+
