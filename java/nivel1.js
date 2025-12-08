86ad1013dc7a36886c0d150d989cfdddbef0a9fb
Your Name <mariaeduarda.dan.avila@gmail.com  git config --global user.email you@example.com  git config --global user.name DudaAvila>
2025-12-08 15:37:59 -0300

    UPDATE

---
+++ java/nivel1.js
@@ -0,0 +1,21 @@
+const obstaculos = [
+    // --- Carro Roxo (Começa na Coluna 10, Índice 9 | Linhas 2, 3, 4)
+    
+    // Linha 3 (Índice 2)
+    {x: 9, y: 2}, {x: 10, y: 2}, 
+    
+    // --- Carro Rosa (Ajuste Fino, usando índices que funcionam visualmente)
+    // Se o Carro Rosa começa visualmente na Coluna 4 (índice 3),
+    {x: 4, y: 5}, {x: 5, y: 5},
+
+    // --- Gato (Ajuste Fino: Coluna 14, Linha 4)
+    {x: 14, y: 4},
+
+    // Muito longe do destino 
+     {x: 18, y: 0}, //linha 0
+
+     //Muito longe do destino 
+      {x: 18, y: 6}, //linha 5
+
+    
+];
